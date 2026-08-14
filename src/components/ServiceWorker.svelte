280642c1a2a51bf3b0739762bcec01d4973b6ab8
Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>
2026-08-14 08:39:06 +0530

    feat(base): break components and improve navbar
    break the base into components with nav, footer and service worker
    
    Signed-off-by: Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>

---
+++ src/components/ServiceWorker.svelte
@@ -0,0 +1,14 @@
+<script>
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js').then(
+        (registration) => {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        },
+        (err) => {
+          console.error('ServiceWorker registration failed: ', err);
+        }
+      );
+    });
+  }
+</script>
